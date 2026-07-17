c4af7c38b1dc35b84a6e88e4cc59fd109d6926c9
Johan Rojas <147665143+JARS2009@users.noreply.github.com>
2026-07-16 19:01:28 -0500

    fix: registrar GpsListenCommand en console.php para Laravel 11

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Console\Commands\GpsListenCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Registrar comando GPS listener (TCP servidor para rastreadores SinoTrack GT06)
+Artisan::resolve(GpsListenCommand::class);
